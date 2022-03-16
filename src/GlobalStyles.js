a235f464aee0df9a686479a0ba133e6e1b3aa98b
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-03-16 20:10:21 +0100

    Alternative box model added

---
+++ src/GlobalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    html{
+        box-sizing: border-box;
+    };
+    *, ::after, ::before {
+        box-sizing: inherit;
+    };
+`;
