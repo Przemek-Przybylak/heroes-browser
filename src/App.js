0e68020bd00f2e5dd48e3e2142d8150980d8f83c
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-03-27 20:53:33 +0200

    added routing in App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import { HerosList } from "./features/herosList/HerosList";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
+import { HerosList } from "./features/herosList";
+import {Hero} from "./features/hero/Hero";
 
 function App() {
   return (
     <div className="App">
-      <HerosList />
+      <HashRouter>
+          <Switch>
+              <Route path={"/heroes/:index"}>
+                  <Hero />
+              </Route>
+              <Route path={"/heroes"}>
+                  <HerosList />
+              </Route>
+              <Route path={"/"}>
+                     <Redirect to={"/heroes"} />
+              </Route>
+          </Switch>
+      </HashRouter>
     </div>
   );
 }
