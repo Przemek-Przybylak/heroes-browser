a0b625d94f0a625a770414ada885f47f9e9c3f86
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-03 18:45:32 +0200

    added watchFetchHero in rootSaga

--- src/core/rootSaga.js
+++ src/core/rootSaga.js
@@ -1,6 +1,7 @@
 import { all } from "@redux-saga/core/effects";
 import { watchFetchHeroesList } from "../features/herosList/heroesListSaga";
+import {watchFetchHero} from "../features/hero/heroSaga";
 
 export default function* rootSaga() {
-    yield all([watchFetchHeroesList(),])
+    yield all([watchFetchHeroesList(), watchFetchHero(),])
 }
