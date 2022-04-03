20e01ed79296ba87bf980ae6654f6515114fd24d
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-03 18:44:29 +0200

    hero from store slice added in Hero component

--- src/features/hero/Hero/index.js
+++ src/features/hero/Hero/index.js
@@ -1,4 +1,17 @@
-export const Hero = (index) => {
+import {useDispatch, useSelector} from "react-redux";
+import {fetchHero, selectHero} from "../heroSlice";
+import {useEffect} from "react";
+
+export const Hero = () => {
+    const dispatch = useDispatch();
+    const currentState = `people/1`;
+    const hero = useSelector(selectHero);
+    console.log(hero)
+
+    useEffect(() => {
+        dispatch(fetchHero(currentState));
+    }, [dispatch]);
+
     return (
         <>
         nanan
