8d8063ee141ff87a5e115bbb97a761ea79096684
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-03 16:59:38 +0200

    heroes map in HeroesList component added

--- src/features/herosList/index.js
+++ src/features/herosList/index.js
@@ -3,6 +3,8 @@ import {useDispatch, useSelector} from "react-redux";
 import {fetchList, selectHeroesList} from "./heroesListSlice";
 import {useEffect} from "react";
 import {Tile} from "../../common/Tile";
+import {Name, TileWrapper} from "../../common/Tile/styled";
+import {StyledLink} from "../../GlobalStyles";
 
 export const HerosList = () => {
     const dispatch = useDispatch();
@@ -16,8 +18,24 @@ export const HerosList = () => {
 
   return (
     <>
-      <Wrapper>
-          <Tile heroes={heroes}/>
+      <Wrapper >
+          {heroes &&
+              heroes.map(({name, birth_year, eye_color, films, gender, height, homeworld, skin_color, starchips, vechicles}) => (
+                  <>
+                      <Tile
+                          name={name}
+                          birth_year={birth_year}
+                          eye_color={eye_color}
+                          films={films}
+                          gender={gender}
+                          height={height}
+                          homeworld={homeworld}
+                          skin_color={skin_color}
+                          starchips={starchips}
+                          vechicles={vechicles}
+                      />
+                  </>
+              ))}
       </Wrapper>
     </>
   );
