df92df4f62bc943c2beff1df88fef4a52e9dcfae
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-03 18:46:07 +0200

    added heroReducer in store

--- src/core/store.js
+++ src/core/store.js
@@ -1,13 +1,15 @@
 import createSagaMiddleware from "redux-saga";
 import {configureStore} from "@reduxjs/toolkit";
 import rootSaga from "./rootSaga";
-import heroesReducer from "../features/herosList/heroesListSlice"
+import heroesReducer from "../features/herosList/heroesListSlice";
+import heroReducer from "../features/hero/heroSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         heroes: heroesReducer,
+        hero: heroReducer,
     },
     middleware: [sagaMiddleware],
 });
