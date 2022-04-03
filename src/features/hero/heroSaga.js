1e7759f508892a84c2207a32df3b54520caabe12
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-03 18:43:41 +0200

    HeroSaga added

---
+++ src/features/hero/heroSaga.js
@@ -0,0 +1,20 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+import {
+    fetchHero,
+    fetchHeroSuccess,
+    fetchHeroError,
+} from "./heroSlice";
+import {fetchData} from "../../common/fetchData";
+
+function* fetchHeroHandler({ payload: currentState }) {
+    try {
+        const hero = yield call( fetchData, currentState );
+        yield put(fetchHeroSuccess(hero));
+    } catch (error) {
+        yield put(fetchHeroError("error"));
+    }
+}
+
+export function* watchFetchHero() {
+    yield takeLatest(fetchHero.type, fetchHeroHandler);
+}
