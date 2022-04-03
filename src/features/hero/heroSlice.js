8acc72b25437ac57ec035361897e28bb8c8ea51f
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-03 18:41:56 +0200

    HeroSlice added

---
+++ src/features/hero/heroSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const peopleSlice = createSlice({
+    name: "hero",
+    initialState: {
+        status: "initial",
+        person: {
+        }
+    },
+    reducers: {
+        fetchHero: (state) => {
+            state.status = "loading";
+        },
+        fetchHeroSuccess: (state, {payload: hero}) => {
+            state.person = hero;
+            state.status = "success";
+        },
+        fetchHeroError: (state) => {
+            state.status = "error";
+        },
+    },
+});
+
+export const {
+    fetchHero,
+    fetchHeroSuccess,
+    fetchHeroError,
+} = peopleSlice.actions;
+
+export const selectHero = (state) => state.hero.person
+
+export default peopleSlice.reducer;
