ee687cf9be9f83d109e8ea7f4325fc8415c3199c
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-04 19:18:18 +0200

    hero bio added to render in Hero component

--- src/features/hero/Hero/index.js
+++ src/features/hero/Hero/index.js
@@ -1,6 +1,7 @@
 import {useDispatch, useSelector} from "react-redux";
 import {fetchHero, selectHero} from "../heroSlice";
 import {useEffect} from "react";
+import {Item, Wrapper} from "./styled";
 
 export const Hero = () => {
     const dispatch = useDispatch();
@@ -13,8 +14,29 @@ export const Hero = () => {
     }, [dispatch]);
 
     return (
-        <>
-        nanan
-        </>
+        <Wrapper>
+            <h1>{hero.name}</h1>
+            <Item>
+                birth in: {hero.birth_year}
+            </Item>
+            <Item>
+                eyes: {hero.eye_color}
+            </Item>
+            <Item>
+                gender: {hero.gender}
+            </Item>
+            <Item>
+                height: {hero.height} cm
+            </Item>
+            <Item>
+                skin color: {hero.skin_color}
+            </Item>
+            <Item>
+                hair: {hero.hair_color}
+            </Item>
+            <Item>
+                mass: {hero.mass} kg
+            </Item>
+        </Wrapper>
     )
 }
