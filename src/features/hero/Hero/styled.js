d90631db0a7d16b4f3c68b00f711ea9a26afe4bd
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-04 19:19:36 +0200

    styles for Hero added

---
+++ src/features/hero/Hero/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  padding: 20px 900px 20px 20px;
+  box-shadow: inset 0 0 1em grey;
+`;
+
+export const Item = styled.p`
+  border: 1px solid black;
+  font-size: 15px;
+  font-weight: 600;
+  text-transform: uppercase;
+  padding: 5px;
+  border-radius: 5%;
+`;
