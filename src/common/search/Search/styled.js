1f765a9ecebfb69232bd64f9d85732ac3ae1f020
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-05 22:20:30 +0200

    added styles to Sesarch component

---
+++ src/common/search/Search/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.header`
+  margin: 0;
+  display: flex;
+  align-items: flex-end;
+`;
+export const Input = styled.input`
+  width: 200px;
+`;
