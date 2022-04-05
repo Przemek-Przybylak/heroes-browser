5be608fb820893892cbf8f0062ebfcae25395e21
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-05 22:18:39 +0200

    added searching interaction

--- src/common/search/Search/index.js
+++ src/common/search/Search/index.js
@@ -1,3 +1,25 @@
-export const Search = () => {
+import {useQueryParameter, useReplaceQueryParameter} from "../queryParameters";
+import {Input, Wrapper} from "./styled";
 
+export const Search = () => {
+    const query = useQueryParameter("search");
+    const replaceQueryParameter = useReplaceQueryParameter();
+    const placeholder = "Search for hero...";
+
+    const onInputChange = ({ target }) => {
+        replaceQueryParameter(
+            {
+                key: "search",
+                value: target.value.trim() !== "" ? target.value : "",
+            }
+        )
+    }
+
+    return (
+        <Wrapper>
+            <Input placeholder={placeholder}
+                   value={query || ""}
+                   onChange={onInputChange} />
+        </Wrapper>
+    )
 }
