13fd6ccb8c72a1cabea892a45020acc99476c487
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-05 22:20:04 +0200

    added custom hooks for query

---
+++ src/common/search/queryParameters.js
@@ -0,0 +1,27 @@
+import { useHistory, useLocation } from "react-router";
+
+export const useQueryParameter = key => {
+    const location = useLocation();
+    const searchParams = new URLSearchParams(location.search);
+    return searchParams.get(key);
+};
+
+export const useReplaceQueryParameter = () => {
+    const location = useLocation();
+    const history = useHistory();
+
+    const searchParams = new URLSearchParams(location.search);
+
+    return ({ key, value }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if(value === undefined) {
+            searchParams.delete(key);
+        } else {
+            searchParams.set(key, value);
+        }
+
+        const newSearch = searchParams.toString();
+        history.push(`${location.pathname}?${newSearch}`)
+    }
+    };
