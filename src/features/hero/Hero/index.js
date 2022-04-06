a041de536b11629d1f45928ee752506fdd6fd0a1
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-06 13:38:40 +0200

    added query to hero fetch in Hero component

--- src/features/hero/Hero/index.js
+++ src/features/hero/Hero/index.js
@@ -1,42 +1,47 @@
 import {useDispatch, useSelector} from "react-redux";
-import {fetchHero, selectHero} from "../heroSlice";
+import {fetchHero, selectHero, selectStatus} from "../heroSlice";
 import {useEffect} from "react";
 import {Item, Wrapper} from "./styled";
+import {StateChecker} from "../../../common/StateChecker";
 
 export const Hero = () => {
+    const query = window.location.hash.substr(8);
     const dispatch = useDispatch();
-    const currentState = `people/1`;
+    const currentState = `people/${query}`;
     const hero = useSelector(selectHero);
-    console.log(hero)
+    const status = useSelector(selectStatus);
 
     useEffect(() => {
         dispatch(fetchHero(currentState));
     }, [dispatch]);
 
     return (
-        <Wrapper>
-            <h1>{hero.name}</h1>
-            <Item>
-                birth in: {hero.birth_year}
-            </Item>
-            <Item>
-                eyes: {hero.eye_color}
-            </Item>
-            <Item>
-                gender: {hero.gender}
-            </Item>
-            <Item>
-                height: {hero.height} cm
-            </Item>
-            <Item>
-                skin color: {hero.skin_color}
-            </Item>
-            <Item>
-                hair: {hero.hair_color}
-            </Item>
-            <Item>
-                mass: {hero.mass} kg
-            </Item>
-        </Wrapper>
+        <StateChecker status={status}>
+            {hero &&
+                <Wrapper>
+                    <h1>{hero.name}</h1>
+                    <Item>
+                        birth in: {hero.birth_year}
+                    </Item>
+                    <Item>
+                        eyes: {hero.eye_color}
+                    </Item>
+                    <Item>
+                        gender: {hero.gender}
+                    </Item>
+                    <Item>
+                        height: {hero.height} cm
+                    </Item>
+                    <Item>
+                        skin color: {hero.skin_color}
+                    </Item>
+                    <Item>
+                        hair: {hero.hair_color}
+                    </Item>
+                    <Item>
+                        mass: {hero.mass} kg
+                    </Item>
+                </Wrapper>}
+        </StateChecker>
     )
 };
