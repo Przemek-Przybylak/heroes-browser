1a3e65fdef9f29fb4ba8bbae5a45819c36c96c24
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-06 10:37:39 +0200

    added styles to Search Component

--- src/common/search/Search/styled.js
+++ src/common/search/Search/styled.js
@@ -3,8 +3,13 @@ import styled from "styled-components";
 export const Wrapper = styled.header`
   margin: 0;
   display: flex;
-  align-items: flex-end;
+  align-items: center;
+  justify-content: flex-end;
+  width: 100%;
+  height: 100px;
 `;
 export const Input = styled.input`
-  width: 200px;
+  width: 300px;
+  padding: 5px;
+  font-size: 20px;
 `;
