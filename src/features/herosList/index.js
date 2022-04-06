a9b77e5b8a068ab459a2e901f2428c876b553424
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-06 10:35:58 +0200

    added selectTaskByQuery action in HerosesList

--- src/features/herosList/index.js
+++ src/features/herosList/index.js
@@ -1,14 +1,16 @@
-import { Wrapper } from "./styled";
+import {Wrapper} from "./styled";
 import {useDispatch, useSelector} from "react-redux";
-import {fetchList, selectHeroesList} from "./heroesListSlice";
+import {fetchList, selectHeroesByQuery, selectHeroesList} from "./heroesListSlice";
 import {useEffect} from "react";
 import {Tile} from "../../common/Tile";
 import {Search} from "../../common/search/Search";
+import {useQueryParameter} from "../../common/queryParameters";
 
 export const HerosList = () => {
     const dispatch = useDispatch();
-    const heroes = useSelector(selectHeroesList);
     const currentState = `people`;
+    const query = useQueryParameter("search")
+    const heroes = useSelector(state => selectHeroesByQuery(state, query));
     console.log(heroes)
 
     useEffect(() => {
@@ -16,29 +18,41 @@ export const HerosList = () => {
     }, [dispatch]);
     console.log("lalala")
 
-  return (
-    <>
-        <Search />
-      <Wrapper >
-          {heroes &&
-              heroes.map(({name, birth_year, eye_color, films, gender, height, homeworld, skin_color, starchips, vechicles, url}) => (
-                  <>
-                      <Tile
-                          name={name}
-                          birth_year={birth_year}
-                          eye_color={eye_color}
-                          films={films}
-                          gender={gender}
-                          height={height}
-                          homeworld={homeworld}
-                          skin_color={skin_color}
-                          starchips={starchips}
-                          vechicles={vechicles}
-                          url={url.substr(29)}
-                      />
-                  </>
-              ))}
-      </Wrapper>
-    </>
-  );
+    return (
+        <>
+            <Search/>
+            <Wrapper>
+                {heroes &&
+                    heroes.map(({
+                                    name,
+                                    birth_year,
+                                    eye_color,
+                                    films,
+                                    gender,
+                                    height,
+                                    homeworld,
+                                    skin_color,
+                                    starchips,
+                                    vechicles,
+                                    url
+                                }) => (
+                        <>
+                            <Tile
+                                name={name}
+                                birth_year={birth_year}
+                                eye_color={eye_color}
+                                films={films}
+                                gender={gender}
+                                height={height}
+                                homeworld={homeworld}
+                                skin_color={skin_color}
+                                starchips={starchips}
+                                vechicles={vechicles}
+                                url={url.substr(29)}
+                            />
+                        </>
+                    ))}
+            </Wrapper>
+        </>
+    );
 };
