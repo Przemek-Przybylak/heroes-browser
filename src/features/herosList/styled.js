2dea0a060630da710d5c13337c373c38d56b8f47
Przemek Przybylak <przemyslaw.przybylak2@gmail.com>
2022-04-06 10:39:12 +0200

    top and botton margin in HeroesList Wrapper deleted

--- src/features/herosList/styled.js
+++ src/features/herosList/styled.js
@@ -1,8 +1,7 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  margin: 50px auto;
-  width: 1000px;
+  margin: 0px auto;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   gap: 30px;
